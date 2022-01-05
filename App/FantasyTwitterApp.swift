43c3f914ad69da49fa6425e16e0a45a0b8449f89
liseami <51239614+liseami@users.noreply.github.com>
2022-01-05 12:41:56 +0800

    站在巨人肩膀上

---
+++ App/FantasyTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyTwitterApp.swift
+//  Shared
+//
+//  Created by 赵翔宇 on 2022/1/5.
+//
+
+import SwiftUI
+
+@main
+struct FantasyTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-FantasyTwitter (iOS)/Pods-FantasyTwitter (iOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FantasyTwitter__iOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_FantasyTwitter__iOS_
+@end
